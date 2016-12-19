ba0cce891b3f6d5f3cd9fa826cb596979990c893
leeka2010 <leeka2010@mail.ru>
2016-12-19 21:35:44 +0300

    HW01

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,7 +1,25 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
 /**
  * Created by ruslangrigoryev on 19.12.16.
  */
-public class MealServlet {
+public class MealServlet extends HttpServlet{
+    private static final Logger LOG = getLogger(MealServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to meals");
+
+//        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+        response.sendRedirect("meals.jsp");
+    }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -8,9 +8,10 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title></title>
+    <title>Meal list</title>
 </head>
 <body>
-
+<h2><a href="index.html">Home</a></h2>
+<h2>Meal list</h2>
 </body>
 </html>
