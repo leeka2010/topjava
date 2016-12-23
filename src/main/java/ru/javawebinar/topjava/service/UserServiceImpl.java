531b0785de5fffced4aa1afda255f0fbae0fa8af
leeka2010 <leeka2010@mail.ru>
2016-12-23 19:00:05 +0300

    spring context

--- src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
@@ -13,8 +15,10 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
  * GKislin
  * 06.03.2015.
  */
+@Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
     private UserRepository repository;
 
     @Override
